5c8b32247ba199c0fb4b9960bf1943c409784a40
zhang-mingming <zhangming.ming@hotmail.com>
2018-11-26 08:57:03 +0800

    硬件串口优化
    对调串口，启用自带控制485

--- Program/switch_three/Src/main.c
+++ Program/switch_three/Src/main.c
@@ -145,7 +145,6 @@ void MX_LPUART1_UART_Init(void)
 /* USART2 init function */
 void MX_USART2_UART_Init(void)
 {
-
 	huart2.Instance = USART2;
 	huart2.Init.BaudRate = 115200;
 	huart2.Init.WordLength = UART_WORDLENGTH_8B;
